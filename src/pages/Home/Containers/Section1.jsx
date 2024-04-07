6e37718148d2293a9e8eee485850cdbc0fad2c57
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-04-07 17:55:01 -0300

    Section 1

---
+++ src/pages/Home/Containers/Section1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Section1.module.css";
+
+export default function Section1() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.content}>
+        <h1>
+          <span>Melhore sua Saúde</span>: Consultas de Fitoterapia e outras PICS
+        </h1>
+        <p>
+          Explore o Poder das Plantas e da Acupuntura Auricular para Transformar
+          sua Saúde e Bem-Estar, onde quer que você esteja!
+        </p>
+        <button>Mais informações</button>
+      </div>
+    </section>
+  );
+}

3c9093d4f3198f6ccbb68a650627f1d1e2aead71
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2024-04-26 19:07:39 -0300

    Daniela - Home - Comentários do Google

--- src/pages/Home/Containers/Section6Pacientes.jsx
+++ src/pages/Home/Containers/Section6Pacientes.jsx
@@ -63,9 +63,10 @@ export default function Section6Pacientes() {
                     <Star />
                   </div>
                   <p>
-                    Dra Daniela é incrível! Direta, objetiva, avalia a
-                    totalidade do ser e seu contexto, explica o que significa o
-                    resultado dos exames e relaciona com os sintomas descritos.
+                    Excelente e diferenciada profissional da saúde holística,
+                    ministra cursos de alto nível e faz atendimentos presenciais
+                    e online! Um privilégio tê-la como parte do quadro de
+                    colaboradores do Instituto Potala!
                   </p>
                   <div className={styles.dataUser}>
                     <div className={styles.profile}>
@@ -73,7 +74,7 @@ export default function Section6Pacientes() {
                     </div>
                     <div className={styles.dataProfile}>
                       <h1>Testemunho #1</h1>
-                      <p>Elaine Fatima Carbonari Pereira</p>
+                      <p>Soraya Sartoran</p>
                     </div>
                     <div className={styles.iconUser}>
                       <img src={aspas} alt="Icone Aspas" />
@@ -100,7 +101,7 @@ export default function Section6Pacientes() {
                       <img src={profile} alt="Icone Perfil" />
                     </div>
                     <div className={styles.dataProfile}>
-                      <h1>Testemunho #1</h1>
+                      <h1>Testemunho #2</h1>
                       <p>Elaine Fatima Carbonari Pereira</p>
                     </div>
                     <div className={styles.iconUser}>
@@ -119,17 +120,17 @@ export default function Section6Pacientes() {
                     <Star />
                   </div>
                   <p>
-                    Dra Daniela é incrível! Direta, objetiva, avalia a
-                    totalidade do ser e seu contexto, explica o que significa o
-                    resultado dos exames e relaciona com os sintomas descritos.
+                    Adorei a consulta com a Dra. Daniela. Muito competente! Tem
+                    o dom da escuta e o tratamento é super eficiente. Que
+                    maravilha a consulta ser online pois sou de outra cidade.
                   </p>
                   <div className={styles.dataUser}>
                     <div className={styles.profile}>
                       <img src={profile} alt="Icone Perfil" />
                     </div>
                     <div className={styles.dataProfile}>
-                      <h1>Testemunho #1</h1>
-                      <p>Elaine Fatima Carbonari Pereira</p>
+                      <h1>Testemunho #3</h1>
+                      <p>Debora Correa</p>
                     </div>
                     <div className={styles.iconUser}>
                       <img src={aspas} alt="Icone Aspas" />
@@ -147,17 +148,18 @@ export default function Section6Pacientes() {
                     <Star />
                   </div>
                   <p>
-                    Dra Daniela é incrível! Direta, objetiva, avalia a
-                    totalidade do ser e seu contexto, explica o que significa o
-                    resultado dos exames e relaciona com os sintomas descritos.
+                    Amei o atendimento, a Doutora Daniela é super atenciosa, bem
+                    explicativa, ela tem interesse a necessidade de cada
+                    cliente, de longe da pra ver que ela ama o que faz, ela não
+                    só atende com experiência, mas também com o coração.
                   </p>
                   <div className={styles.dataUser}>
                     <div className={styles.profile}>
                       <img src={profile} alt="Icone Perfil" />
                     </div>
                     <div className={styles.dataProfile}>
-                      <h1>Testemunho #1</h1>
-                      <p>Elaine Fatima Carbonari Pereira</p>
+                      <h1>Testemunho #4</h1>
+                      <p>Jaqueline Meireles</p>
                     </div>
                     <div className={styles.iconUser}>
                       <img src={aspas} alt="Icone Aspas" />
@@ -175,17 +177,99 @@ export default function Section6Pacientes() {
                     <Star />
                   </div>
                   <p>
-                    Dra Daniela é incrível! Direta, objetiva, avalia a
-                    totalidade do ser e seu contexto, explica o que significa o
-                    resultado dos exames e relaciona com os sintomas descritos.
+                    Atendimento espetacular! Superou todas as minhas
+                    expectativas! Super recomendo! Profissional extremamente
+                    dedicada, com grande expertise na área da Saúde.
                   </p>
                   <div className={styles.dataUser}>
                     <div className={styles.profile}>
                       <img src={profile} alt="Icone Perfil" />
                     </div>
                     <div className={styles.dataProfile}>
-                      <h1>Testemunho #1</h1>
-                      <p>Elaine Fatima Carbonari Pereira</p>
+                      <h1>Testemunho #5</h1>
+                      <p>Juliana Candido</p>
+                    </div>
+                    <div className={styles.iconUser}>
+                      <img src={aspas} alt="Icone Aspas" />
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={styles.swiperCard}>
+                <div className={styles.card}>
+                  <div className={styles.stars}>
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                  </div>
+                  <p>
+                    Profissional muito competente, explica tudo para o paciente.
+                    Gostei muito e realmente a fitoterapia tem me ajudado a
+                    solução de meus problemas.
+                  </p>
+                  <div className={styles.dataUser}>
+                    <div className={styles.profile}>
+                      <img src={profile} alt="Icone Perfil" />
+                    </div>
+                    <div className={styles.dataProfile}>
+                      <h1>Testemunho #6</h1>
+                      <p>Sabrine Gomes</p>
+                    </div>
+                    <div className={styles.iconUser}>
+                      <img src={aspas} alt="Icone Aspas" />
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={styles.swiperCard}>
+                <div className={styles.card}>
+                  <div className={styles.stars}>
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                  </div>
+                  <p>
+                    Uma ótima profissional, cuidado com carinho e atenção,
+                    tratou a família inteira. Recomendo.
+                  </p>
+                  <div className={styles.dataUser}>
+                    <div className={styles.profile}>
+                      <img src={profile} alt="Icone Perfil" />
+                    </div>
+                    <div className={styles.dataProfile}>
+                      <h1>Testemunho #7</h1>
+                      <p>Paula Andrea</p>
+                    </div>
+                    <div className={styles.iconUser}>
+                      <img src={aspas} alt="Icone Aspas" />
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide className={styles.swiperCard}>
+                <div className={styles.card}>
+                  <div className={styles.stars}>
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                    <Star />
+                  </div>
+                  <p>
+                    Profissional incrível!!! Obrigada por toda sua atenção e
+                    conhecimento
+                  </p>
+                  <div className={styles.dataUser}>
+                    <div className={styles.profile}>
+                      <img src={profile} alt="Icone Perfil" />
+                    </div>
+                    <div className={styles.dataProfile}>
+                      <h1>Testemunho #8</h1>
+                      <p>Inara Merique</p>
                     </div>
                     <div className={styles.iconUser}>
                       <img src={aspas} alt="Icone Aspas" />
